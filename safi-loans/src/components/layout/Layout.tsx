e646297d2fa8aa298cbc9937bc296e18bc0a1852
😂 <you@example.com>
2026-04-19 17:02:24 +0300

    Remove WhatsApp floating button

--- safi-loans/src/components/layout/Layout.tsx
+++ safi-loans/src/components/layout/Layout.tsx
@@ -1,14 +1,12 @@
 import { ReactNode } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import WhatsAppButton from "./WhatsAppButton";
 
 const Layout = ({ children }: { children: ReactNode }) => (
   <div className="flex min-h-screen flex-col">
     <Navbar />
     <main className="flex-1">{children}</main>
     <Footer />
-    <WhatsAppButton />
   </div>
 );
 
